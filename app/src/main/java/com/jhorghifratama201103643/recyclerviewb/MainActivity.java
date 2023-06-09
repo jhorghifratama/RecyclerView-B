bb5cdfe5b731f0d1ef13dfd8f43457fe264a93e1
jhorghifratama <jhorghifratama@gmail.com>
2023-06-09 15:02:33 +0700

    viewB

--- app/src/main/java/com/jhorghifratama201103643/recyclerviewb/MainActivity.java
+++ app/src/main/java/com/jhorghifratama201103643/recyclerviewb/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.loopj.android.http.AsyncHttpClient;
@@ -18,7 +19,10 @@ import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private FloatingActionButton _addButton;
+
     private RecyclerView _recyclerView1;
 
     @Override
@@ -28,6 +32,13 @@ public class MainActivity extends AppCompatActivity {
 
         _recyclerView1 = (RecyclerView) findViewById(R.id.recyclerView1);
 
+
+
+        initAddButton();
+        loadRecyclerView();
+
+    }
+    private void loadRecyclerView(){
         AsyncHttpClient ahc = new AsyncHttpClient();
         String url = "https://stmikpontianak.net/011100862/tampilMahasiswa.php";
 
@@ -49,6 +60,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
                 Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+    private void initAddButton()
+    {
+        _addButton = findViewById(R.id.addButton);
+
+        _addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),AddMahasiswaActivity.class);
+                startActivity(intent);
+
+                loadRecyclerView();
             }
         });
     }
